c4949f2a6a161c065237b507fc3f5a384d295604
chetuu16 <sanchisbeldajose@gmail.com>
2019-06-20 17:43:14 +0200

    setup the base page

---
+++ frontend/src/Componentes/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Importacion de estilos
+import './Header.css';
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div>
+                Esto es el header
+            </div>
+        )
+    }
+}
+export default Header;
---
+++ frontend/src/Vistas/Index/Index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//Importacion de componentes
+import Header from '../../Componentes/Header/Header';
+
+//Importacion de estilos
+import './Index.css';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Index;
